4960641ba5b3f5e4ff8009536275f07108f3613c
IordanisK <danniskarassavas@gmail.com>
2022-03-11 17:14:04 +0100

    folder structure

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 //React navigation stack 
-import RootStack from './navigators/RootStack';
+import RootStack from '../flower_crib/client/navigators/RootStack';
 
 export default function App() {
   return (
